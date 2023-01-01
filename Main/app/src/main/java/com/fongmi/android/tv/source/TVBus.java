e1b8b860fd887fe64d9ffa3c5df81520ffbb37e4
FongMi <b8892778@gmail.com>
2023-01-02 01:19:15 +0800

    Fix crash for t

--- Main/app/src/main/java/com/fongmi/android/tv/source/TVBus.java
+++ Main/app/src/main/java/com/fongmi/android/tv/source/TVBus.java
@@ -1,5 +1,6 @@
 package com.fongmi.android.tv.source;
 
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 
@@ -9,90 +10,87 @@ import com.fongmi.android.tv.impl.AsyncCallback;
 import com.google.android.exoplayer2.PlaybackException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.tvbus.engine.Listener;
 import com.tvbus.engine.TVCore;
-import com.tvbus.engine.TVListener;
-import com.tvbus.engine.TVService;
 
-public class TVBus implements TVListener {
+public class TVBus implements Listener {
 
-	private final Handler handler;
-	private AsyncCallback callback;
-	private TVCore tvcore;
+    private final Handler handler;
+    private AsyncCallback callback;
+    private TVCore tvcore;
 
-	private static class Loader {
-		static volatile TVBus INSTANCE = new TVBus();
-	}
+    private static class Loader {
+        static volatile TVBus INSTANCE = new TVBus();
+    }
 
-	public static TVBus get() {
-		return Loader.INSTANCE;
-	}
+    public static TVBus get() {
+        return Loader.INSTANCE;
+    }
 
-	public TVBus() {
-		this.handler = new Handler(Looper.getMainLooper());
-	}
+    public TVBus() {
+        this.handler = new Handler(Looper.getMainLooper());
+    }
 
-	public void init(Core core) {
-		tvcore = TVCore.getInstance();
-		tvcore.setAuthUrl(core.getAuth());
-		tvcore.setUsername(core.getName());
-		tvcore.setPassword(core.getPass());
-		tvcore.setMKBroker(core.getBroker());
-		tvcore.setTVListener(this);
-		TVService.start(App.get());
-	}
+    public void init(Core core) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) return;
+        tvcore = new TVCore().listener(this);
+        tvcore.auth(core.getAuth()).name(core.getName()).pass(core.getPass()).broker(core.getBroker());
+        tvcore.serv(0).play(8902).mode(1).init(App.get());
+    }
 
-	public void start(AsyncCallback callback, String url) {
-		setCallback(callback);
-		tvcore.start(url);
-	}
+    public void start(AsyncCallback callback, String url) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) return;
+        setCallback(callback);
+        tvcore.start(url);
+    }
 
-	public void stop() {
-		if (tvcore != null) tvcore.stop();
-	}
+    public void stop() {
+        if (tvcore != null) tvcore.stop();
+    }
 
-	public void destroy() {
-		stop(); TVService.stop(App.get());
-	}
+    public void destroy() {
+        if (tvcore != null) tvcore.quit();
+    }
 
-	private void setCallback(AsyncCallback callback) {
-		this.callback = callback;
-	}
+    private void setCallback(AsyncCallback callback) {
+        this.callback = callback;
+    }
 
-	@Override
-	public void onPrepared(String result) {
-		JsonObject json = new Gson().fromJson(result, JsonObject.class);
-		if (json.get("hls") == null) return;
-		handler.post(() -> onResponse(json.get("hls").getAsString()));
-	}
+    @Override
+    public void onPrepared(String result) {
+        JsonObject json = new Gson().fromJson(result, JsonObject.class);
+        if (json.get("hls") == null) return;
+        handler.post(() -> onResponse(json.get("hls").getAsString()));
+    }
 
-	@Override
-	public void onStop(String result) {
-		JsonObject json = new Gson().fromJson(result, JsonObject.class);
-		int errno = json.get("errno").getAsInt();
-		if (errno < 0) handler.post(this::onError);
-	}
+    @Override
+    public void onStop(String result) {
+        JsonObject json = new Gson().fromJson(result, JsonObject.class);
+        int errno = json.get("errno").getAsInt();
+        if (errno < 0) handler.post(this::onError);
+    }
 
-	private void onResponse(String result) {
-		if (callback != null) callback.onResponse(result);
-	}
+    private void onResponse(String result) {
+        if (callback != null) callback.onResponse(result);
+    }
 
-	private void onError() {
-		if (callback != null) callback.onError(new PlaybackException(null, null, 0));
-	}
+    private void onError() {
+        if (callback != null) callback.onError(new PlaybackException(null, null, 0));
+    }
 
-	@Override
-	public void onInited(String result) {
-	}
+    @Override
+    public void onInited(String result) {
+    }
 
-	@Override
-	public void onStart(String result) {
-	}
+    @Override
+    public void onStart(String result) {
+    }
 
-	@Override
-	public void onInfo(String result) {
-	}
+    @Override
+    public void onInfo(String result) {
+    }
 
-	@Override
-	public void onQuit(String result) {
-	}
+    @Override
+    public void onQuit(String result) {
+    }
 }
